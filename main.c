1cc2639481b6a38163862381d8bc04a40c799906
prapapornyin <prapaporn.yin@student.mahidol.edu>
2021-03-04 14:35:35 +0000

    edit all

--- main.c
+++ main.c
@@ -6,8 +6,9 @@
 int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
   int i,N,j;
-  
   Stack s;
+  s.size = 0;
+  s.top = NULL;
 
 
  
@@ -15,15 +16,36 @@ int main(int argc, char **argv){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
-
-
+       N =strlen(argv[i]);
+       // {[ -->push
+       // }] -->pop + compare
+       if(argv[i][j] == '{' || argv[i][j] == '[')
+       {
+         push(&s,argv[i][j]);
+       }
+       if(argv[i][j] == '}' || argv[i][j] == ']')
+       {
+         pop(&s);
+       }
+              
      }
-
+     if(argv[i][j] == argv[i][N]){
+        if(s.size == 0){
+          printf("The parentheses match successfull for ");for(j=0;j<strlen(argv[i]);j++){
+            printf("%c",argv[i][j]);
+          }
+        } 
+        else {
+          printf("The parentheses do not match for ");
+          for(j=0;j<strlen(argv[i]);j++){
+            printf("%c",argv[i][j]);
+          }
+        }
+        printf("\n");
+        pop_all(&s);
+     }
+     
 
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -1,5 +1,5 @@
 
-#ifndef stack_h
+//#ifndef stack_h
 #define stack_h
 #include "node.h"
 typedef struct {
@@ -8,7 +8,39 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(Stack s);
-void pop_all(Stack s);
-#endif
+void push(StackPtr s, char value){
+  //printf("%c\n",value);
+  //size = 0;
+  Node * new_node = (Node *)malloc(sizeof(Node));
+  if(new_node){
+    new_node->data = value;
+    new_node->nextPtr = s->top;
+    s->top = new_node;
+    s->size = s->size+1;
+  }
+  //printf("push\n");
+  //printf("%d\n",s->size);
+}
+char pop(StackPtr s){
+  if(s->size != 0){
+    NodePtr t = s->top;
+    int value;
+    t = s->top;
+    value = t->data;
+    s->top = t->nextPtr;
+    free(t);
+    s->size = s->size-1;
+    //printf("%d\n",s->size);
+  }
+  else s->size = s->size+1;
+  //printf("pop\n");
+  //printf("%d\n",s->size);
+}
+void pop_all(StackPtr s){ 
+
+  s->size = 0;
+  s->top = NULL;
+
+  //printf("clear all\n");
+}
+// เลิกใช้ clear หมด
